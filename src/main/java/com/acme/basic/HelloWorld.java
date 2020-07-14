bc03e9c524d04097bb409c90c341ba8d60d1aedd
drgs <drgs@users.noreply.github.com>
2020-07-14 13:33:26 +0200

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -9,5 +9,21 @@ public class HelloWorld {
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
+  
+  // void sayHello() {
+  //  System.out.println("Hello World!");
+  // }
+
+  // void notCovered() {
+  //  System.out.println("This method is not covered by unit tests");
+  // }
+  
+  // void sayHello() {
+  //  System.out.println("Hello World!");
+  // }
+
+  // void notCovered() {
+  //  System.out.println("This method is not covered by unit tests");
+  //}
 
 }

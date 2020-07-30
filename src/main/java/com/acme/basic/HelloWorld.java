107a3aef2c9a475e29b013759302d785962c4829
drgs <drgs@users.noreply.github.com>
2020-07-30 17:54:58 +0200

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
   
   public String sayHello() {
     System.out.println("Hello World!");
-    
+    int cinci = 5;
     if (l.size() == 0 && (1 + 1) == 2) {
       return "...";
     }

ca150e1d6defe86b2d1b1ecaef53b4209fe77662
drgs <drgs@users.noreply.github.com>
2020-08-04 13:41:18 +0200

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -1,9 +1,16 @@
 package com.acme.basic;
 
 public class HelloWorld {
-
+  String testStr = "test";
+  
   void sayHello() {
     System.out.println("Hello World!");
+    System.out.println(testStr.equals("test"));
+    
+    testStr = null;
+    System.out.println(testStr.equals("test"));
+    
+    System.out.println("test".equals(testStr));
   }
 
   void notCovered() {

c0d70873cb9c21019ef2dbbdfe84306a6fce485e
drgs <drgs@users.noreply.github.com>
2020-08-04 15:06:38 +0200

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -1,13 +1,20 @@
 package com.acme.basic;
 
-public class HelloWorld {
+public class A() {
+ 	public A() {
+		
+	}
+	public void foo() {
+		System.out.println("foo");
+	} 
+}
 
+public class HelloWorld {
+	@SuppressWarnings({"java:S1104"})
+	public static A instance = new A();
+  
   void sayHello() {
     System.out.println("Hello World!");
-  }
-
-  void notCovered() {
-    System.out.println("This method is not covered by unit tests");
   }
 
 }

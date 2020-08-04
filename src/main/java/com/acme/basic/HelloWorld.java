a927413219b5016b3db648ffcff097843b75ed93
drgs <drgs@users.noreply.github.com>
2020-08-04 15:13:43 +0200

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -1,12 +1,5 @@
 package com.acme.basic;
 
-public class A {
- 	public A() {}
-	public void foo() {
-		System.out.println("foo");
-	} 
-}
-
 public class HelloWorld {
   @SuppressWarnings({"java:S1104"})
   public static A instance = new A();
